98c0b90cd11ec79a9c9a8bba816ab3e76a2f6007
SaiBalaji2001 <gadisaibalaji2001@gmail.com>
2023-03-28 22:43:19 -0500

    modified users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+let more = 0;
+let sum =0;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more = more + 1;
+  sum = more;
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
